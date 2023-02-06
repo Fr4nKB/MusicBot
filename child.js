ce3c6b87510e9c9ab4447b24a57a85f0223ba3bd
Fr4nKB <70409297+Fr4nKB@users.noreply.github.com>
2023-02-06 21:10:11 +0100

    Update child.js

--- child.js
+++ child.js
@@ -2,6 +2,13 @@ const { execSync } = require('child_process');
 
 //extracts title and URL and returns it to the main process
 process.on('message', (line) => {
-    const res = execSync('youtube-dl -o "%(title)s//%(id)s" --get-filename ytsearch:"'+line+'"', { encoding: 'utf-8' });
+    var res;
+    try {   //line is an URL
+        const temp = new URL(line);
+        res = execSync('yt-dlp -o "%(title)s//youtu.be/%(id)s//%(duration)s" --get-filename --no-warnings '+line, { encoding: 'utf-8' });
+    } catch(error) {    //line is the title of a song
+        line.replace(/[^0-9a-z\s]/gi, '');    //sanitization, only alphanumeric + spaces
+        res = execSync('yt-dlp -o "%(title)s//youtu.be/%(id)s//%(duration)s" --get-filename --no-warnings ytsearch:"'+line+'"', { encoding: 'utf-8' });
+    }
     process.send(res);
 });
